39df20ee809ca252ba9c50cb0bf211ceb0642eec
Sulabh Shrestha <Shresthasulabh777@gmail.com>
2023-08-03 22:15:40 +0545

    Fixed: page replacement issue

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mood_tracker/models/time_stamp.dart';
-import 'package:mood_tracker/providers/homepage_key_provider.dart';
 import 'package:mood_tracker/view_models/mood_list_view_model.dart';
 import 'package:mood_tracker/view_models/user_view_model.dart';
 import 'package:mood_tracker/views/add_new_mood/add_new_mood.dart';
@@ -22,152 +21,130 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
-  final homePageGlobalKey = GlobalKey<_HomePageState>();
-
-  @override
-  void initState() {
-    initializeProvider();
-    super.initState();
-  }
-
-  void initializeProvider() {
-    Future(() {
-      log("Homekey: ${homePageGlobalKey.currentContext}");
-      // For displaying alert dialog using homepage buildContext
-      ref.read(homePageKeyProvider.notifier).state = homePageGlobalKey;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    initializeProvider();
-    return Navigator(
-      key: homePageGlobalKey,
-      onGenerateRoute: (settings) => MaterialPageRoute(
-        builder: (context) => Stack(
-          children: [
-            // Content
-            NestedScrollView(
-              floatHeaderSlivers: true,
-              headerSliverBuilder: (_, isScrolled) {
-                return [
-                  SliverAppBar(
-                    floating: true,
-                    title: Text(
-                      "Hey! ${UserViewModel().getUserName.split(" ").first}",
-                      style: const TextStyle(color: Colors.black),
-                    ),
-                    actions: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => const SearchPage()));
-                        },
-                        icon: const Icon(Icons.search),
-                      ),
-                    ],
+    return Stack(
+      children: [
+        // Content
+        NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (_, isScrolled) {
+            return [
+              SliverAppBar(
+                floating: true,
+                title: Text(
+                  "Hey! ${UserViewModel().getUserName.split(" ").first}",
+                  style: const TextStyle(color: Colors.black),
+                ),
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      // hiding nav bar
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const SearchPage()));
+                    },
+                    icon: const Icon(Icons.search),
                   ),
-                ];
-              },
-              body: StreamBuilder<QuerySnapshot>(
-                stream: MoodListViewModel().moodsStream,
-                builder: (context, snapshot) {
-                  log("data: ${snapshot.hasData} && ${snapshot.connectionState}, ${snapshot.data?.docs}");
+                ],
+              ),
+            ];
+          },
+          body: StreamBuilder<QuerySnapshot>(
+            stream: MoodListViewModel().moodsStream,
+            builder: (context, snapshot) {
+              log("data: ${snapshot.hasData} && ${snapshot.connectionState}, ${snapshot.data?.docs}");
 
-                  if (snapshot.hasData &&
-                      snapshot.connectionState == ConnectionState.active) {
-                    List<QueryDocumentSnapshot> listData =
-                        snapshot.data!.docs.toList();
+              if (snapshot.hasData &&
+                  snapshot.connectionState == ConnectionState.active) {
+                List<QueryDocumentSnapshot> listData =
+                    snapshot.data!.docs.toList();
 
-                    final groupedData =
-                        <String, List<QueryDocumentSnapshot>>{}; // date: List
+                final groupedData =
+                    <String, List<QueryDocumentSnapshot>>{}; // date: List
 
-                    for (var document in listData) {
-                      final date = document.get("date");
-                      if (!groupedData.containsKey(date)) {
-                        groupedData[date] = [];
-                      }
-                      groupedData[date]?.add(document);
+                for (var document in listData) {
+                  final date = document.get("date");
+                  if (!groupedData.containsKey(date)) {
+                    groupedData[date] = [];
+                  }
+                  groupedData[date]?.add(document);
+                }
+
+                return ListView(
+                  children: List.generate(groupedData.entries.length, (index) {
+                    List<QueryDocumentSnapshot> values =
+                        groupedData.entries.elementAt(index).value;
+
+                    // Labelling date {today, yesterday, tomorrow }
+                    var dateLabel = DateHelperUtils()
+                        .getDateLabel(values.first.get('date'));
+
+                    if (values.length == 1) {
+                      return SingleItemCard(
+                        date: values.first.get("date"),
+                        dateLabel: dateLabel,
+                        rating: values.first.get("rating"),
+                        timeStamp: TimeStamp(values.first.get("timestamp")),
+                        reason: values.first.get("why"),
+                        feedback: values.first.get("feedback"),
+                        dbImagesPath: values.first.get("imagesPath"),
+                      );
                     }
 
-                    return ListView(
-                      children:
-                          List.generate(groupedData.entries.length, (index) {
-                        List<QueryDocumentSnapshot> values =
-                            groupedData.entries.elementAt(index).value;
+                    // Means we have more than one mood in a single day
+                    else {
+                      List<String?> feedbacks = [];
+                      List<int> ratings = [];
+                      List<TimeStamp> timestamps = [];
+                      List<String?> reasons = [];
+                      List<List<dynamic>> imagesStoragePaths =
+                          []; // List of array of paths
 
-                        // Labelling date {today, yesterday, tomorrow }
-                        var dateLabel = DateHelperUtils()
-                            .getDateLabel(values.first.get('date'));
+                      // Combining all the data
+                      for (var element in values) {
+                        feedbacks.add(element.get("feedback"));
+                        ratings.add(element.get("rating"));
+                        timestamps.add(TimeStamp(element.get("timestamp")));
+                        reasons.add(element.get("why"));
 
-                        if (values.length == 1) {
-                          return SingleItemCard(
-                            date: values.first.get("date"),
-                            dateLabel: dateLabel,
-                            rating: values.first.get("rating"),
-                            timeStamp: TimeStamp(values.first.get("timestamp")),
-                            reason: values.first.get("why"),
-                            feedback: values.first.get("feedback"),
-                            dbImagesPath: values.first.get("imagesPath"),
-                          );
-                        }
+                        var imagesPath = element.get("imagesPath");
 
-                        // Means we have more than one mood in a single day
-                        else {
-                          List<String?> feedbacks = [];
-                          List<int> ratings = [];
-                          List<TimeStamp> timestamps = [];
-                          List<String?> reasons = [];
-                          List<List<dynamic>> imagesStoragePaths =
-                              []; // List of array of paths
+                        imagesStoragePaths.add(imagesPath);
+                      }
 
-                          // Combining all the data
-                          for (var element in values) {
-                            feedbacks.add(element.get("feedback"));
-                            ratings.add(element.get("rating"));
-                            timestamps.add(TimeStamp(element.get("timestamp")));
-                            reasons.add(element.get("why"));
+                      return MultiItemCard(
+                        date: values.first.get("date"),
+                        dateLabel: dateLabel,
+                        feedbacks: feedbacks,
+                        ratings: ratings,
+                        reasons: reasons,
+                        timeStamps: timestamps,
+                        imagesStoragePaths: imagesStoragePaths,
+                      );
+                    }
+                  }),
+                );
+              }
 
-                            var imagesPath = element.get("imagesPath");
-
-                            imagesStoragePaths.add(imagesPath);
-                          }
-
-                          return MultiItemCard(
-                            date: values.first.get("date"),
-                            dateLabel: dateLabel,
-                            feedbacks: feedbacks,
-                            ratings: ratings,
-                            reasons: reasons,
-                            timeStamps: timestamps,
-                            imagesStoragePaths: imagesStoragePaths,
-                          );
-                        }
-                      }),
-                    );
-                  }
-
-                  return const Center(child: CircularProgressIndicator());
-                },
-              ),
-            ),
-
-            // Navigate to [AddNewMood] page when click
-            Positioned(
-              bottom: 20,
-              right: 20,
-              child: FloatingActionButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const AddNewMood()),
-                  );
-                },
-                child: const Icon(Icons.add),
-              ),
-            ),
-          ],
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
         ),
-      ),
+
+        // Navigate to [AddNewMood] page when click
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const AddNewMood()),
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/views/root_page/root_page.dart
+++ lib/views/root_page/root_page.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:mood_tracker/providers/homepage_key_provider.dart';
 import 'package:mood_tracker/views/home_page/home_page.dart';
 import 'package:mood_tracker/views/more_page/more_page.dart';
 
@@ -11,14 +15,14 @@ import 'utils/interaction_utils.dart';
 /// This page is the main page, that hols [HomePage], [Calendar] and [Stats] pages
 ///
 
-class RootPage extends StatefulWidget {
+class RootPage extends ConsumerStatefulWidget {
   const RootPage({Key? key}) : super(key: key);
 
   @override
-  State<RootPage> createState() => _RootPageState();
+  ConsumerState<RootPage> createState() => _RootPageState();
 }
 
-class _RootPageState extends State<RootPage> {
+class _RootPageState extends ConsumerState<RootPage> {
   // Storing list of pages
   List<Widget> _pages = [];
 
@@ -27,6 +31,8 @@ class _RootPageState extends State<RootPage> {
 
   ConnectivityResult? connectivityResult;
   bool isDialogShown = false;
+
+  final homePageGlobalKey = GlobalKey<_RootPageState>();
 
   @override
   void initState() {
@@ -51,8 +57,18 @@ class _RootPageState extends State<RootPage> {
     setState(() {});
   }
 
+  void initializeProvider() {
+    Future(() {
+      log("Homekey: ${homePageGlobalKey.currentContext}");
+      // For displaying alert dialog using homepage buildContext
+      ref.read(homePageKeyProvider.notifier).state = homePageGlobalKey;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    initializeProvider();
+
     if (connectivityResult == ConnectivityResult.none &&
         connectivityResult != null) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -68,73 +84,77 @@ class _RootPageState extends State<RootPage> {
                 builder: (context) => doYouWantToExit(context)) ??
             false;
       },
-      child: Scaffold(
-        body: PageView.builder(
-          onPageChanged: (page) {
-            setState(() {
-              selectedIndex = page;
-            });
-          },
-          controller: controller,
-          itemCount: _pages.length,
-          itemBuilder: (context, index) {
-            return _pages[index];
-          },
-        ),
-        bottomNavigationBar: SafeArea(
-          child: Container(
-            margin: const EdgeInsets.only(top: 5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  spreadRadius: -10,
-                  blurRadius: 60,
-                  color: Colors.black.withOpacity(.4),
-                  offset: const Offset(0, 25),
-                )
-              ],
+      child: Navigator(
+        key: homePageGlobalKey,
+        onGenerateRoute: (settings) => MaterialPageRoute(builder: (context) {
+          return Scaffold(
+            body: PageView.builder(
+              onPageChanged: (page) {
+                setState(() {
+                  selectedIndex = page;
+                });
+              },
+              controller: controller,
+              itemCount: _pages.length,
+              itemBuilder: (context, index) {
+                return _pages[index];
+              },
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 3.0, vertical: 3),
-              child: GNav(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                color: Colors.grey.shade800,
-                tabActiveBorder: Border.all(),
-                activeColor: Colors.black,
-                iconSize: 24,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
-                gap: 10,
-                tabs: const [
-                  GButton(
-                    icon: Icons.home,
-                    text: 'Home',
-                  ),
-                  GButton(
-                    icon: Icons.calendar_month,
-                    text: 'Calendar',
-                  ),
-                  GButton(
-                    icon: Icons.filter_alt_outlined,
-                    text: 'Filter',
-                  ),
-                  GButton(
-                    icon: Icons.more_horiz,
-                    text: 'More',
-                  ),
+            bottomNavigationBar: Container(
+              margin: const EdgeInsets.only(top: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: -10,
+                    blurRadius: 60,
+                    color: Colors.black.withOpacity(.4),
+                    offset: const Offset(0, 25),
+                  )
                 ],
-                selectedIndex: selectedIndex,
-                onTabChange: (index) {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                  controller.jumpToPage(index);
-                },
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 3.0, vertical: 3),
+                child: GNav(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  color: Colors.grey.shade800,
+                  tabActiveBorder: Border.all(),
+                  activeColor: Colors.black,
+                  iconSize: 24,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+                  gap: 10,
+                  tabs: const [
+                    GButton(
+                      icon: Icons.home,
+                      text: 'Home',
+                    ),
+                    GButton(
+                      icon: Icons.calendar_month,
+                      text: 'Calendar',
+                    ),
+                    GButton(
+                      icon: Icons.filter_alt_outlined,
+                      text: 'Filter',
+                    ),
+                    GButton(
+                      icon: Icons.more_horiz,
+                      text: 'More',
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onTabChange: (index) {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                    controller.jumpToPage(index);
+                  },
+                ),
               ),
             ),
-          ),
-        ),
+          );
+        }),
       ),
     );
   }
