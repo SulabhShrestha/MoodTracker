e32089573c23ec7dadaacc10311aa929b89b96f5
Sulabh Shrestha <Shresthasulabh777@gmail.com>
2023-10-03 22:57:28 +0545

    DataDeletePage: Fixed UI

--- lib/views/more_page/pages/data_delete_page.dart
+++ lib/views/more_page/pages/data_delete_page.dart
@@ -16,81 +16,85 @@ class DataDeletePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Delete my data"),
       ),
-      body: Column(
-        children: [
-          CustomBox(
-            child: Column(children: [
-              CustomListTile(
-                title: "Delete my account",
-                leadingIconData: Icons.account_box,
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (_) => DeleteConfirmationDialog(
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Text(
-                            "Are you sure you want to delete your account?",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            "Warning: This will delete all your existing data and cannot be undone.",
-                            style: TextStyle(
-                              color: Constant().colors.red,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+        child: Column(
+          children: [
+            CustomBox(
+              child: Column(children: [
+                CustomListTile(
+                  title: "Delete my account",
+                  leadingIconData: Icons.account_box,
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => DeleteConfirmationDialog(
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Text(
+                              "Are you sure you want to delete your account?",
+                              style: TextStyle(fontSize: 18),
                             ),
-                          ),
-                        ],
-                      ),
-                      onConfirm: () async {
-                        await UserViewModel().deleteUser();
-                      },
-                    ),
-                  );
-                },
-              ),
-            ]),
-          ),
-          CustomBox(
-            child: Column(children: [
-              CustomListTile(
-                title: "Delete my data",
-                leadingIconData: Icons.share_outlined,
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (_) => DeleteConfirmationDialog(
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Text(
-                            "Are you sure you want to delete your data?",
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            "Warning: This will delete all your existing data, account and cannot be undone.",
-                            style: TextStyle(
-                              color: Constant().colors.red,
+                            const SizedBox(height: 8),
+                            Text(
+                              "Warning: This will delete all your existing data and cannot be undone.",
+                              style: TextStyle(
+                                color: Constant().colors.red,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
+                        onConfirm: () async {
+                          await UserViewModel().deleteUser();
+                        },
                       ),
-                      onConfirm: () async {
-                        UserViewModel().deleteUserData().then((val) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text("Deleted successfully")));
-                        });
-                      },
-                    ),
-                  );
-                },
-              ),
-            ]),
-          ),
-        ],
+                    );
+                  },
+                ),
+              ]),
+            ),
+            Constant().spaces.vertical12(),
+            CustomBox(
+              child: Column(children: [
+                CustomListTile(
+                  title: "Delete my data",
+                  leadingIconData: Icons.share_outlined,
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => DeleteConfirmationDialog(
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Text(
+                              "Are you sure you want to delete your data?",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              "Warning: This will delete all your existing data, account and cannot be undone.",
+                              style: TextStyle(
+                                color: Constant().colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                        onConfirm: () async {
+                          UserViewModel().deleteUserData().then((val) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text("Deleted successfully")));
+                          });
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ]),
+            ),
+          ],
+        ),
       ),
     );
   }
